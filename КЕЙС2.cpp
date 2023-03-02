e193f24cbf1911811b879d0289923261921e8683
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-03-02 21:30:39 +0300

    Add files via upload

---
+++ КЕЙС2.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+vector <int> sa(vector <int> mass){
+    int s = mass[0];
+    mass[0] = mass[1];
+    mass[1] = s;
+    return mass;
+}
+
+void ra(vector <int> &mass){ // 23451
+    int k = mass[0];
+    for(int i = 0; i < mass.size(); i++){
+        mass[i] = mass[i+1];
+    }
+    mass[mass.size()-1] = k;
+}
+
+void rra(vector <int> &mass){ // 51234
+    if(mass.size() == 0){
+    }else{
+        int a = mass[mass.size()-1];
+        for(int i = mass.size()-1; i > 0; i--){
+            mass[i] = mass[i-1];
+        }
+        mass[0] = a;
+    }
+}
+
+void pb(vector <int> &mass1, vector <int> &mass2, int msz1, int msz2){
+    if(msz2 == 0){
+        mass2[0] = mass1[0];
+        msz2++;
+    }if(msz2 == 1){
+        mass2[1] = mass2[0];
+        mass2[0] = mass1[0];
+        msz2++;
+    }else{
+        vector <int> massd = mass2;
+        for(int i = 0; i < msz2; i++){
+            mass2[i+1] = massd[i];
+        }
+        mass2[0] = mass1[0];
+        msz2++;
+    }
+    ra(mass1);
+    msz1--;
+}
+
+int main(){
+    vector <int> massA;
+    vector <int> massB;
+    string ss;
+    int a = 0;
+    int i = 0;
+    while(a != 1){
+        cin >> ss;
+        int s = stoi(ss);
+        massA[i] = s;
+    }
+
+    int mszA = massA.size();
+    int mszB = massB.size();
+
+    cout << "grebannie massive A, blay" << endl;
+    for(int i = 0; i < massA.size(); i++){
+        cout << massA[i] << endl;
+    }
+}
+
