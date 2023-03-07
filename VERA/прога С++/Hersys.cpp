e12a176f946c99ef76425cdcb4d2b6342b4838bf
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-03-07 12:09:50 +0300

    Add files via upload

--- VERA/прога С++/Hersys.cpp
+++ VERA/прога С++/Hersys.cpp
@@ -1,38 +1,100 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<vector>
-
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-void ra(vector <int> massA) // ra
-{
-if(massA.size()==0)
-{
-}
-else
-{
-int a = massA[massA.size()-1];
-for(int i=massA.size()-1; i>0;i--)
-{
-massA[i] = massA[i-1];
-}
-massA[0] = a;
-}
-} // end
 
 
-int main()
-{
-vector <int> massA(3);
-massA[0] = 1;
-massA[1] = 2;
-massA[2] = 3;
-massA = ra(massA);
-cout << massA[0];
-return 0;
+int main(){
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ VERA/прога питони/awejiopk.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+
+f = a * b
+print(f // 2 + f % 2)
