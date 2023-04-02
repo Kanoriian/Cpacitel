d2560fb4a7fe71c0fd5515244f9458422ea4e815
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-04-02 18:38:07 +0300

    Update SorticShakerComb.cpp

--- VERA/прога С++/SorticShakerComb.cpp
+++ VERA/прога С++/SorticShakerComb.cpp
@@ -45,34 +45,19 @@ void combsort(vector <int> &mass){
 }
 
 int main(){
-    vector <int> mass(20);
-    mass[0] = 7;
-    mass[1] = 10;
-    mass[2] = 20;
-    mass[3] = 4;
-    mass[4] = 6;
-    mass[5] = 16;
-    mass[6] = 18;
-    mass[7] = 15;
-    mass[8] = 3;
-    mass[9] = 11;
-    mass[10] = 17;
-    mass[11] = 14;
-    mass[12] = 13;
-    mass[13] = 2;
-    mass[14] = 1;
-    mass[15] = 8;
-    mass[16] = 12;
-    mass[17] = 5;
-    mass[18] = 9;
-    mass[19] = 19;
-
-    shakersort(mass);
+    vector <int> mass;
+    int msz, a;
+    cout << "enter the length of the array: ";
+    cin >> msz;
+    cout << "enter the array numbers:" << endl;
+    for(int i = 0; i < msz; i++){
+        cin >> a;
+        mass.push_back(a);
+    }
 
     combsort(mass);
 
     for(int i = 0; i < mass.size(); i++){
         cout << mass[i] << " ";
-        cout << "idiot";
     }
 }
